08af168461fb019f433751742641d85855d7a63a
Norathat Jongjitnant <norathat@gmail.com>
2020-09-17 00:30:21 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,18 @@
+function takeANumber(katzDeliLine, name){
+  katzDeliLine.push(name)
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
+}
+
+function nowServing(katzDeliLine){
+  if (katzDeliLine.length == 0) {
+    return "There is nobody waiting to be served!"
+  } else {
+    let serving = katzDeliLine[0]
+    katzDeliLine.shift()
+    return `Currently serving ${serving}.`
+  }
+}
+
+function currentLine(katzDeliLine){
+
+}
